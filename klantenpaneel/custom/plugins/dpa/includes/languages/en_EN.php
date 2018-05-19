6ce1de87ea11635a82402b1c0ee081f737f91ced
William David Edwards <WEdwards@cyberfusion.nl>
2018-05-19 12:40:46 +0200

    Identation fixes

--- klantenpaneel/custom/plugins/dpa/includes/languages/en_EN.php
+++ klantenpaneel/custom/plugins/dpa/includes/languages/en_EN.php
@@ -14,13 +14,13 @@ $LANG['Dpa accepted title']             = 'Data Processing Agreement accepted';
 $LANG['Dpa accept title']               = 'Accept';
 
 /** MESSAGES */
-$LANG['iframe error']	                  = 'An error occurred while loading the Data Processing Agreement.';
-$LANG['signed soon']	                  = 'The Data Processing Agreement can be signed soon.';
-$LANG['success']	                      = 'Successfully accepted Data Processing Agreement.';
-$LANG['error processing']	              = 'A problem occurred while accepting the agreement. Please contact us.';
-$LANG['not checked']	                  = 'Data Processing Agreement was not accepted.';
+$LANG['iframe error']                   = 'An error occurred while loading the Data Processing Agreement.';
+$LANG['signed soon']                    = 'The Data Processing Agreement can be signed soon.';
+$LANG['success']                        = 'Successfully accepted Data Processing Agreement.';
+$LANG['error processing']               = 'A problem occurred while accepting the agreement. Please contact us.';
+$LANG['not checked']                    = 'Data Processing Agreement was not accepted.';
 $LANG['already agreed']                 = 'The Data Processing Agreement has already been accepted for this account.';
-$LANG['agree to dpa']	                  = 'I agree to the Data Processing Agreement.';
+$LANG['agree to dpa']                   = 'I agree to the Data Processing Agreement.';
 $LANG['download pdf']                   = 'Download PDF';
 $LANG['download pdf here']              = 'Click here to download the Data Processing Agreement in PDF.';
 $LANG['accept']                         = 'Accept agreement';
