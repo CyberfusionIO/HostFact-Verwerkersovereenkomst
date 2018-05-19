dbc619200df8881636340d10c741e27b4477fb4e
William David Edwards <WEdwards@cyberfusion.nl>
2018-05-19 12:32:28 +0200

    Styling fixes

--- klantenpaneel/custom/plugins/dpa/models/dpa_model.php
+++ klantenpaneel/custom/plugins/dpa/models/dpa_model.php
@@ -16,11 +16,11 @@ class Dpa_Model extends \Base_Model
 
 	public $config;
 
-    /**
-    * dpa::__construct()
-    *
-    * @return void
-    */
+	/**
+	* dpa::__construct()
+	*
+	* @return void
+	*/
 	public function __construct()
 	{
 		$this->Error = $this->Warning = $this->Success = array();
@@ -31,11 +31,11 @@ class Dpa_Model extends \Base_Model
         	$this->config = isset($config) ? $config : [];
 	}
 
-    /**
-    * Check if DPA plugin is active (if PDF doc exists).
-     *
-    * @return boolean
-    */
+	/**
+	* Check if DPA plugin is active (if PDF doc exists).
+	*
+	* @return boolean
+	*/
 	public function isActivated()
     	{
 		// Config input entered.
@@ -59,11 +59,11 @@ class Dpa_Model extends \Base_Model
         	}
 	}
 
-    /**
-    * Get current Debtor identifier.
-     *
-    * @return int
-    */
+	/**
+	* Get current Debtor identifier.
+	*
+	* @return int
+	*/
 	public function getCurrentDebtor()
 	{
 		$debtor_model = new \Debtor_Model();
@@ -75,11 +75,11 @@ class Dpa_Model extends \Base_Model
 		return $debtor_model->Identifier;
 	}
 
-    /**
-    * Send email to Debtor if successfully agreed.
-     *
-    * @return boolean
-    */
+	/**
+	* Send email to Debtor if successfully agreed.
+	*
+	* @return boolean
+	*/
 	public function sendEmail($debtorid) {		
 		$debtorParams = array(
 			'Identifier'	=> $debtorid,
@@ -96,57 +96,57 @@ class Dpa_Model extends \Base_Model
         	}
 	}
 
-    /**
-     * Process signing
-     *
-     * @param int $debtor
-     * @param boolean $signing
-     * @return array
-     */
-    public function processSignDate($debtor) {
-        // Determine sign date
-        $sign_date = date("d-m-Y H:i") . ' (' . $_SERVER['REMOTE_ADDR'] . ')';
+	/**
+	* Process signing
+	*
+	* @param int $debtor
+	* @param boolean $signing
+	* @return array
+	*/
+	public function processSignDate($debtor) {
+        	// Determine sign date
+        	$sign_date = date("d-m-Y H:i") . ' (' . $_SERVER['REMOTE_ADDR'] . ')';
 
-        // Edit debtor custom field with sign date
-        $response = $this->APIRequest('debtor', 'edit', array('Identifier' => $debtor, 'CustomFields' => array($this->config['fieldname'] => $sign_date)));
+        	// Edit debtor custom field with sign date
+        	$response = $this->APIRequest('debtor', 'edit', array('Identifier' => $debtor, 'CustomFields' => array($this->config['fieldname'] => $sign_date)));
 
-        return $response;
-    }
+        	return $response;
+	}
 
-    /**
-     * Edit Debtor with DPA accept date.
-     *
-     * @return boolean
-     */
-    public function updatePreference() {
-        $debtor = $this->getCurrentDebtor();
-        $response = $this->processSignDate($debtor, true);
-
-        // Process signing.
-        if($response['status'] == "success") {
-
-            // Send email if an email address is available. If error occurred, ignore (as if debtor has no emailaddress).
-            if (isset($response['debtor']['EmailAddress']) && $response['debtor']['EmailAddress'] != '') {
-                $this->sendEmail($debtor);
-            }
-
-            return true;
-        }
-
-        // Error processing signing.
-        else {
-            return false;
-        }
-    }
-
-    /**
-    * Retrieve debtor DPA info.
-     *
-    * @return string
-    */
-	public function debtorDPAStatus() {
+	/**
+	* Edit Debtor with DPA accept date.
+	*
+	* @return boolean
+	*/
+	public function updatePreference() {
         	$debtor = $this->getCurrentDebtor();
-        	$response = $this->APIRequest('debtor', 'show', array('Identifier' => $debtor));
+        	$response = $this->processSignDate($debtor, true);
+
+        	// Process signing.
+        	if ($response['status'] == "success") {
+
+            		// Send email if an email address is available. If error occurred, ignore (as if debtor has no emailaddress).
+            		if (isset($response['debtor']['EmailAddress']) && $response['debtor']['EmailAddress'] != '') {
+                		$this->sendEmail($debtor);
+            		}
+
+            		return true;
+        	}
+
+        	// Error processing signing.
+        	else {
+            		return false;
+        	}
+	}
+
+	/**
+	* Retrieve debtor DPA info.
+	*
+	* @return string
+	*/
+	public function debtorDPAStatus() {
+		$debtor = $this->getCurrentDebtor();
+		$response = $this->APIRequest('debtor', 'show', array('Identifier' => $debtor));
 
         	// If custom field is filled, debtor agreed already
 		if (isset($response['debtor']['CustomFields'][$this->config['fieldname']]) && $response['debtor']['CustomFields'][$this->config['fieldname']] != "") {
@@ -156,5 +156,4 @@ class Dpa_Model extends \Base_Model
 			return '';
 		}
 	}
-
 }
