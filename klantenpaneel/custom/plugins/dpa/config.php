8f74766c38494a18776ef9a742502595d34c8878
William David Edwards <WEdwards@cyberfusion.nl>
2018-05-19 13:28:57 +0200

    Fixed comment

--- klantenpaneel/custom/plugins/dpa/config.php
+++ klantenpaneel/custom/plugins/dpa/config.php
@@ -8,5 +8,5 @@ $config['fieldname'] = 'replaceme';
 // Email template ID (edit email template to see the ID in the URL)
 $config['templateid'] = 'replaceme';
 
-// File name of the PDF in the directory 'docs'
+// File name of the PDF in the directory 'docs' (including .pdf extension)
 $config['pdffile'] = 'replaceme';
